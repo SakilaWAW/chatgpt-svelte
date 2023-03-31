33c24c12c82c548331f936365d576075edd3cdbe
Raiden <pccp0909@gmail.com>
2023-03-31 17:52:11 +0800

    finish basic usage of chatgpt api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Configuration, OpenAIApi } from 'openai';
+
+  let input, apiKey;
+
+  async function send() {
+    const configuration = new Configuration({apiKey});
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: input,
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    });
+    input = input + response.data.choices[0].text;
+  }
+</script>
+
+<style>
+  textarea {
+    padding: 0.5em;
+  }
+  button {
+    padding: 0.5em;
+  }
+</style>
+
+<textarea type="text" bind:value={input} />
+<button on:click={send}>Send</button>
+<input type="text" bind:value={apiKey} placeholder="apiKey"/>
